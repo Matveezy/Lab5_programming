be2bc9b399723e80a6b6da92cb498de5324e9984
Matvey Chukhno <mr.chucno@yandex.ru>
2022-01-25 13:55:13 +0300

    fix some issues(NPE when user doesn't choose env var responsibility)

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -2,12 +2,17 @@ package run;
 
 public class Main {
     public static void main(String[] args) {
-    String inputFile = System.getenv("FILE_PATH");
+        try {
+            String inputFile = System.getenv("FILE_PATH");
+            Application application = new Application();
+            application.start(inputFile);
 
-    System.out.println(inputFile);
+        } catch (NullPointerException e){
+            System.err.println("Укажите переменную окружения FILE_PATH!");
+        }
 
-        Application application = new Application();
-        application.start(inputFile);
+//    System.out.println(inputFile);
+
 
     }
 }
